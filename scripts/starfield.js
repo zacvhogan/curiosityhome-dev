364d7b8abb7c2bbe6b214f84b02a46daa9809dd3
Zac Hogan <zacvanhogan@gmail.com>
2023-02-27 13:07:02 +1300

    fix: fix overflows for canvas background, prevent page overflow/scrolling

--- scripts/starfield.js
+++ scripts/starfield.js
@@ -1,85 +1,78 @@
-// set Canvas size
-/// update on window resize
-
-// Generate stars
-
-// Generate milky way - add to stars array
-
-// Render and animate stars
-
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
+// Using screen dimensions instead of viewport
+// This gives a much larger canvas that should not result
+// in the stars hitting canvas borders prematurely and creating artifacts.
+// Simple solution, removes need to additional logic.
+// TODO: Edge case - page opened on browser window on lower resolution monitor
+// then moved to higher resolution monitor results in canvas too small.
+let winWidth = window.screen.width;
+let winHeight = window.screen.height;
+console.log(window.screen.availWidth);
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+canvas.height = winHeight;
+canvas.width = winWidth;
 
-let numStars = (canvas.height * canvas.width) / 400;
+
+let numStars = Math.round((winHeight * winWidth) / 400);
 let starsArray = [];
 
 generateStars(numStars);
-//generateMilkyWay(numStars);
 renderStars(starsArray);
 
 setInterval(animateStars, 50);
 
 function generateStars(numStars) {
 
-	for(let i = 0; i < numStars; i++){
-  	let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-    let size = Math.random() * 2;
-    let color = [Math.random() * (420 - 240) + 240, 100, 100];
-     
+  for (let i = 0; i < numStars; i++) {
+  	
+    let x = Math.random() * winWidth;
+    let y = Math.random() * winHeight;
+    let size = Math.random() * 2.5;
+    let color = `hsl(${Math.round(Math.random() * (340 - 180) + 180)} 100% 80%)`;
+    //console.log(color);
+
+
     starsArray.push([x, y, size, color]);
   }
 }
+console.log(starsArray[0][3]);
 
-function generateMilkyWay(numStars){
 
-	for(let i = 0; i < numStars; i++){
-    let x = Math.random() * (window.innerWidth/2 - 500) + 500;
-      let y = Math.random() * (window.innerHeight/2 - 100) + 100;
-  let size = Math.random() * 2;
-  let color = 'blue';
-    starsArray.push([x, y, size, color]);
-    }
-}
+function renderStars(starsArray) {
 
-function renderStars(starsArray){
+  // Clear previous frame
+  context.clearRect(0, 0, winWidth, winHeight);
 
-	// Clear previous frame
-	context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-	starsArray.forEach(element => {    
-  	// Colour randomisation - in what format??
-  	//context.fillStyle = `hsl(${element[3][0]},${element[3][1]},${element[3][2]})`;    
-    context.fillStyle = "white";
-    let dimChance = Math.random() * 100;
-    if (dimChance < 1){
-    	context.fillStyle="silver";
-    }
-    context.fillRect(element[0], element[1], element[2], element[2]);  
-  });  
-}
-
-function animateStars() {	
-  
-  // Generate new frame
- 	// Set new stars location
   starsArray.forEach(element => {
-  	element[0] = element[0] + 0.5;
-    element[1] = element[1] + 0.1;  
-    
-    if (element[0] > window.innerWidth){
-    	element[0] = 0;    	
+    context.fillStyle = element[3];
+    let dimChance = Math.random() * 100;
+    if (dimChance < 1) {
+      context.fillStyle = "grey";
     }
-    if (element[1] > window.innerHeight){
-    	element[1] = 0;    	
+    context.fillRect(element[0], element[1], element[2], element[2]);
+  });
+}
+
+function animateStars() {
+
+  // Generate new frame
+  // Set new stars location
+  starsArray.forEach(element => {
+    element[0] = element[0] - 0.5;
+    element[1] = element[1] + 0.1;
+
+    if (element[0] < 0) {
+      element[0] = winWidth;
+    }
+    if (element[1] > winHeight) {
+      element[1] = 0;
     }
   });
-  
+
   // Render
   renderStars(starsArray);
-  
+
 
 }
